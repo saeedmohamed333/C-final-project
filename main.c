ffc8d0683933ac8c7cc9f6e53d87e2d48b2a39ae
Saeed <saedhassan290@gmail.com>
2024-08-08 02:10:55 +0300

    Adding main file

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*** TASK1 ***/
+//FUNCTION1: Add new patient
+//FUNCTION2: Edit patient record
+
+/*** TASK2 ***/
+//FUNCTION3: Reserve a slot
+//FUNCTION4: Cancel reservation
+
+/*** TASK3 ***/
+//FUNCTION5: View patient record
+//FUNCTION6: View today's reservation
+
+/*** TASK4 ***/
+//SYSTEM NAVIGATION
+//CODE ASSEMBLY
+
+
+
+int main()
+{
+
+
+    return 0;
+}
